1a0df15f5f326425fdb9c32f02f2d5b141cdce56
kehinde ogundeyi <kehindeogundeyi@gmail.com>
2022-03-12 18:03:33 +0200

    configure CD for the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+  cnstructor(){
+    super();
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +25,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
